b43d4309cc4fe20e3644abe4eee4d38b73a41e95
Sidiq Moltafet <sidiq@MacBook-Air.local>
2023-08-25 17:14:22 -0700

    changed MyProjects to Publications

--- src/components/MainCards.js
+++ src/components/MainCards.js
@@ -10,7 +10,7 @@ import Link from "@mui/joy/Link";
 export default function MainCard() {
   const data = [
     {
-      title: "My Projects",
+      title: "Publications",
       image:
         "https://images.unsplash.com/photo-1527219525722-f9767a7f2884?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
       link: "/#projects",
